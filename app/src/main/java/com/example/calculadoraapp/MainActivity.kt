4c8aba3d279d8ddaf99c8a92e39dbdb05926ac1f
JuanPineda09 <juanpineda420@gmail.com>
2025-03-02 08:49:51 -0500

    firt commit

---
+++ app/src/main/java/com/example/calculadoraapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.calculadoraapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import com.example.calculadoraapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+
+}
